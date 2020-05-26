4f323551906e711dac4bc677e6006a3cbcf8656d
Yusuf Demirci <demirciy94@gmail.com>
2020-05-26 16:10:48 +0300

    - Added example project.

---
+++ Example/SweetLike/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SweetLike
+//
+//  Created by demirciy on 07/29/2019.
+//  Copyright (c) 2019 demirciy. All rights reserved.
+//
+
+import UIKit
+import SweetLike
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sweetLikeButton: SweetLike!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 
 gem "xcpretty"
+gem "cocoapods"
